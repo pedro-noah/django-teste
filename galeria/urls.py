82c303f6f93b663e278e546676a1f15e60e1a497
Noah <pedromilarski11@gmail.com>
2024-05-02 15:25:39 -0300

    url-corretas

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from galeria.views import index
 
 urlpatterns = [
-    path('', index)
+    path('', index),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-        return HttpResponse('<h1>Alura Space</h1><p>Bem-vindo</p>')
+        return render(request, 'index.html')
